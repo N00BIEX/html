b3393f0476400917b22610465281d385f6a7a098
N00BIEX <111552378+N00BIEX@users.noreply.github.com>
2024-01-05 11:51:13 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,51 @@
+const cards = [
+  {
+    image: "https://www.minecraft.net/content/dam/games/minecraft/key-art/Games_Subnav_Minecraft-300x465.jpg",
+    title: "Minecraft",
+    text: "Minecraft is a game where you can build anything you can imagine. Explore endless worlds and build amazing things with your friends. Download Minecraft today and start your adventure!",
+    link: "./html-files/mine_download.html"
+  },
+  {
+    image: "./html-files/images/p1.jpeg",
+    title: "PPS Lab Files",
+    text: "Download the PPS (Programming for Problem Solving) lab files in C language with output, as per the CSVTU syllabus. The files are available in a ZIP format and need to be extracted before use.",
+    link: "./html-files/pps_files.html"
+  },
+  {
+    image: "./html-files/images/r1.jpg",
+    title: "CSVTU Result",
+    text: "To check the result, students need to have their CSVTU login credentials. They can log in using their username and password. The result will show the student's total marks, grade, CP, credit, SIP, and CPI.",
+    link: "./html-files/result3rdsemcsvtugecjdp1.html"
+  },
+  {
+    image: "./html-files/images/pyq1.jpg",
+    title: "CSVTU PYQP",
+    text: "Previous year question paper for students of CSVTU university B.Tech program. These papers are a valuable resource for students who are preparing for their exams.",
+    link: "./html-files/pypq.html"
+  },
+  {
+    image: "./html-files/images/n1.jpeg",
+    title: "Notes",
+    text: "Downloadable content of notes, books, previous year question papers, and hand-written notes for 2<sup>nd</sup> year (3<sup>rd</sup> semester, and 4<sup>th</sup> semester) students by Balkrishna.",
+    link: "https://miningworld21.blogspot.com/?m=1"
+  },
+  {
+    image: "./html-files/images/on1.jpeg",
+    title: "Overview Notes",
+    text: "Downloadable content of online notes for 2<sup>nd</sup> year (3<sup>rd</sup> semester, and 4<sup>th</sup> semester) students by Bard AI. Give an overview of all topics; it's not complete but helpful.",
+    link: "https://drive.google.com/drive/folders/1tqCSvM3UylfsUH9mnMpaGP4dFiW_PVzP"
+  },
+  {
+    image: "./html-files/images/T1.jpg",
+    title: "Telegram Notes",
+    text: "Telegram channel where all notes for 3<sup>rd</sup> year & and 4<sup>th</sup> year students. Provided with notes, books pdf, and PYPQ.",
+    link: "https://t.me/Ajaychandra9111911635"
+  },
+  {
+    image: "./html-files/images/note_down.jpg",
+    title: "Notes",
+    text: "N00BIEX serves as a comprehensive digital repository of meticulously curated academic resources. It grants unfettered access to an extensive collection of meticulously prepared notes and digitized textbooks in PDF format.",
+    link: "./html-files/notes_download.html"
+  }
+  // Add more objects for additional cards
+];
