c42ac5a20cc228a9b76b1d20cd675b39219eecce
N00BIEX <111552378+N00BIEX@users.noreply.github.com>
2025-01-03 13:04:27 +0530

    data.js

--- html-files/js/data.js
+++ html-files/js/data.js
@@ -1,18 +1,19 @@
-const cards = [{
+const cards = [
+    {
     image: "./images/qp1.jpg",
-    title: "Semster 3 CT1",
+    title: "Semster 3 CT1(Mining)",
     text: "CT (Class Test) 1 Question Paper  GECJDP 3rd Semester Year 2022",
     link: "https://drive.google.com/uc?export=download&id=17SYPdJpkoOvRr0QMqOIid8r1EI80V09Z",
-},
+    },
     {
         image: "./images/qp1.jpg",
-        title: "Semester 3 CT2",
+        title: "Semester 3 CT2(Mining)",
         text: "CT (Class Test) 2 Question Paper  GECJDP 3rd Semester Year 2022",
         link: "https://drive.google.com/uc?export=download&id=17XOedrsjNZ61LCyteR5g6aXTq-BaOYtu",
     },
     {
         image: "./images/qp1.jpg",
-        title: "Semester 3 End Semester",
+        title: "Semester 3 End Semester(Mining)",
         text: "End Semester Question Paper  CSVTU 3rd Semester Year 2022",
         link: "https://drive.google.com/uc?export=download&id=17kHHwGoHchtmfA1SX9Q2HpkUHKucMtsA",
     },
@@ -25,52 +26,59 @@ const cards = [{
 
     {
         image: "./images/qp1.jpg",
-        title: "Semster 4 CT1",
+        title: "Semster 4 CT1(Mining)",
         text: "CT (Class Test) 1 Question Paper  GECJDP 4th Semester Year 2023",
         link: "https://drive.google.com/uc?export=download&id=17uMGcG_ls-3KWhNHCidM0yD96Ca1HvaC",
     },
     {
         image: "./images/qp1.jpg",
-        title: "Semester 4 CT2",
+        title: "Semester 4 CT2(Mining)",
         text: "CT (Class Test) 2 Question Paper  GECJDP 4th Semester Year 2022",
         link: "https://drive.google.com/uc?export=download&id=17w5qmp9-C5SIAC_eBgK9k1WZenG1VV9P",
     },
     {
         image: "./images/qp1.jpg",
-        title: "Semester 4 End Semester",
+        title: "Semester 4 End Semester(Mining)",
         text: "End Semester Question Paper  CSVTU 4th Semester Year 2022",
         link: "https://drive.google.com/uc?export=download&id=187PvGW1I5IsvvvREWsGr7Dhy1A-yvwNB",
     },
     {
         image: "./images/qp1.jpg",
-        title: "Semster 5 CT1",
+        title: "Semster 5 CT1(Mining)",
         text: "CT (Class Test) 1 Question Paper  GECJDP 5th Semester Year 2023",
         link: "https://drive.google.com/uc?export=download&id=17xykFIe5yDvWe__k2PDaaOIHzOB3ICz8",
     },
     {
         image: "./images/qp1.jpg",
-        title: "Semester 5 CT2",
+        title: "Semester 5 CT2(Mining)",
         text: "CT (Class Test) 2 Question Paper  GECJDP 5th Semester Year 2023",
         link: "https://drive.google.com/uc?export=download&id=18-WihxyPT847O6EDCtcsMT-d2GrSn7FE",
     },
     {
         image: "./images/qp1.jpg",
-        title: "Semester 5 CT3",
+        title: "Semester 5 CT3(Mining)",
         text: "CT (Class Test) 3 Question Paper  GECJDP 5th Semester Year 2023",
         link: "https://drive.google.com/uc?export=download&id=17yStcpalfIXPTlOubAxjCV5vF3Y2vVjE",
     },
     {
         image: "./images/qp1.jpg",
-        title: "Semester 5 End Semester",
+        title: "Semester 5 End Semester(Mining)",
         text: "End Semester Question Paper  CSVTU 5th Semester Year 2022",
         link: "https://drive.google.com/uc?export=download&id=18Ght9kHOiCYnvNyjs5ynk2EvxXW6QLpo",
     },
     {
         image: "./images/qp1.jpg",
-        title: "Semester 6 End Semester 2024",
+        title: "Semester 6 End Semester 2024(Mining)",
         text: "End Semester Question Paper  CSVTU 6th Semester Year 2024",
         link: "https://drive.google.com/uc?export=download&id=15-ZPaneIMFxDQnnVOWGXZXro7A63wawI",
     },
+    {
+        image: "./images/qp1.jpg",
+        title: "Semester 7 End Semester 2024(Mining)",
+        text: "End Semester Question Paper  CSVTU 7th Semester Year 2024",
+        link: "https://drive.google.com/uc?export=download&id=1yeSZh2HFhq05krYjyDLvPR1UaWwo5nas",
+    },
+               
     // Add more objects for additional cards
 
 ];
