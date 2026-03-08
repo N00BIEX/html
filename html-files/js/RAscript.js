ed6fcf72b2dd3621d404758348c7bd663b164198
N00BIEX <111552378+N00BIEX@users.noreply.github.com>
2026-03-09 00:06:55 +0530

    2026 update detained

--- html-files/js/RAscript.js
+++ html-files/js/RAscript.js
@@ -1,130 +1,84 @@
+const currentCentury = new Date().getFullYear().toString().slice(0, 2);
 
-        var year = 20;
-        var yearR = 0;
+// Function to make checkboxes behave like radios (only one selected)
+// function onlyOne(checkbox) {
+//     const checkboxes = [document.getElementById('RTRV'), document.getElementById('RRV')];
+//     checkboxes.forEach((item) => {
+//         if (item !== checkbox) item.checked = false;
+//     });
+// }
 
-
-        function rollNo_task() {
-            var roll_no = document.getElementById("rollNo").value;
-            var sem = document.getElementById("mySem").value;
-
-            var year1 = roll_no.slice(7, 9);
-            var yearR = parseInt(year1);
-            var lateral_alter = roll_no.slice(9, 12);
-            if (parseInt(lateral_alter) > 100) {
-                yearR--;
-            }
-            return [roll_no,
-                sem,
-                yearR,
-                year];
-        }
-
-        function regular_() {
-            var [roll_no,
-                sem,
-                yearR,
-                year] = rollNo_task();
-            if (sem % 2 === 0) {
-                yearR += sem / 2;
-                month = "apr-may";
-            } else {
-                yearR += (sem - 1) / 2;
-                month = "nov-dec";
-            }
-            var yearRR = year + "" + yearR;
-            return [roll_no,
-                sem,
-                month,
-                yearRR,
-                yearR];
-        }
-
-        function backlog_() {
-            var [roll_no,
-                sem,
-                YearR,
-                year] = rollNo_task();
-            var month = document.getElementById("months").value;
-            var yearR = document.getElementById("yearBack").value;
-            if (yearR > 1900) {
-                yearRR = yearR;
-            } else if (yearR < 10) {
-                var a = new Date().getFullYear().toString();
-                yearRR = a.slice(0, 2) + "0" + yearR;
-            } else {
-                yearRR = year + "" + yearR;
-            }
-            backAdmitYear = document.getElementById("yearBack").value;
-            if (backAdmitYear) {
-                yearR = backAdmitYear.slice(2);
-            }
-            return [roll_no,
-                sem,
-                month,
-                yearRR,
-                yearR];
-        }
-
-        function showInputField() {
-            var inputField = document.getElementById("inputField");
-            var Backlog = document.getElementById("Backlog");
-            var Regular = document.getElementById("Regular");
-
-            if (Backlog.checked) {
-                inputField.style.display = "block";
-            } else if (Regular.checked) {
-                inputField.style.display = "none";
-            }
-        }
-
-        function admit_search() {
-            var Backlog = document.getElementById("Backlog");
-            var Regular = document.getElementById("Regular");
-            var [roll_no,
-                sem,
-                month,
-                yearRR,
-                yearR] = Backlog.checked ? backlog_(): Regular.checked ? regular_(): [];
-            if (roll_no && sem && month && yearRR) {
-                var AdmitLink = "https://csvtu.digivarsity.online/WebApp/Examination/AdmitCard.aspx?RollNo=" + roll_no + "&ExamSession=" + month + "%20" + yearRR + "&Semester=" + sem + "%20SEMESTER";
-                if (roll_no == '300803921049'){
-                        AdmitLink = "https://n00biex.github.io/html/html-files/AdmitCardaspxRollNo300803921049ExamSessionAprMay2024Semester.html"
-                }
-                //alert(AdmitLink);
-                var newTab = window.open(AdmitLink, '_blank')
-                // Focus on the new tab to make it the active tab
-                newTab.focus();
-            }
-        }
-
-        function result_search() {
-            var Backlog = document.getElementById("Backlog");
-            var Regular = document.getElementById("Regular");
-            var [roll_no,
-                sem,
-                month,
-                yearRR] = Backlog.checked ? backlog_(): Regular.checked ? regular_(): [];
-            if (roll_no && sem && month && yearRR) {
-                var selectedOption = document.querySelector('input[name="option"]:checked');
-                var type = selectedOption.value;
-
-            
-                const checkboxRTRV = document.getElementById('RTRV');
-                const checkboxRRV = document.getElementById('RRV');
-
-                if (checkboxRTRV.checked) {
-                    var type = checkboxRTRV.value;
-                }
-
-                else if (checkboxRRV.checked) {
-                    var type = checkboxRRV.value;
-                }
-
-                var ResultLink = "https://csvtu.digivarsity.online/WebApp/Result/SemesterResult.aspx?S=" + sem + "%20SEMESTER&E=" + month + "%20" + yearRR + "&R=" + roll_no + "&T=" + type;
-                //alert(ResultLink);
-                var newTab = window.open(ResultLink, '_blank')
-                // Focus on the new tab to make it the active tab
-                newTab.focus();
-            }
-        }
+function getFormData() {
+    const rollNo = document.getElementById("rollNo").value.trim();
+    const sem = document.getElementById("mySem").value;
+    const isBacklog = document.getElementById("Backlog").checked;
+    const isDetained = document.getElementById("isDetained").checked;
     
+    let month, yearRR;
+
+    if (!rollNo || !sem) {
+        alert("Please enter Roll Number and Semester.");
+        return null;
+    }
+
+    if (isBacklog) {
+        month = document.getElementById("months").value;
+        let yearInput = document.getElementById("yearBack").value;
+        if (!yearInput) { alert("Enter Backlog Year"); return null; }
+        yearRR = yearInput.toString().length === 4 ? yearInput : currentCentury + yearInput.padStart(2, '0');
+    } else {
+        const admissionYearShort = rollNo.slice(7, 9);
+        let yearValue = parseInt(admissionYearShort);
+        if (parseInt(rollNo.slice(9, 12)) > 100) yearValue--; 
+
+        if (sem % 2 === 0) {
+            yearValue += sem / 2;
+            month = "APR-MAY";
+        } else {
+            yearValue += (sem - 1) / 2;
+            month = "NOV-DEC";
+        }
+        if (isDetained) yearValue++;
+        yearRR = currentCentury + yearValue.toString().padStart(2, '0');
+    }
+    return { rollNo, sem, month, yearRR, isBacklog };
+}
+
+function result_search() {
+    const data = getFormData();
+    if (!data) return;
+
+    // Logic for T parameter
+    let type = "Regular";
+    
+    if (data.isBacklog) {
+        type = "Backlog"; // Force Backlog when tab is active
+    }
+
+    // Checkboxes override the type if selected
+    if (document.getElementById('RTRV').checked) {
+        type = "RTRV";
+    } else if (document.getElementById('RRV').checked) {
+        type = "RRV";
+    }
+
+    const ResultLink = `https://csvtu.digivarsity.online/WebApp/Result/SemesterResult.aspx?S=${data.sem}%20Semester&E=${data.month}%20${data.yearRR}&R=${data.rollNo}&T=${type}`;
+    
+    // window.open(ResultLink, '_blank').focus();
+    alert(ResultLink)
+}
+
+function admit_search() {
+    const data = getFormData();
+    if (!data) return;
+
+    // Preserve your specific bypass
+    if (data.rollNo === '300803921049') {
+        window.open("https://n00biex.github.io/html/html-files/AdmitCardaspxRollNo300803921049ExamSessionAprMay2024Semester.html", '_blank').focus();
+        return;
+    }
+
+    const AdmitLink = `https://csvtu.digivarsity.online/WebApp/Examination/AdmitCard.aspx?RollNo=${data.rollNo}&ExamSession=${data.month}%20${data.yearRR}&Semester=${data.sem}%20SEMESTER`;
+    
+    window.open(AdmitLink, '_blank').focus();
+}
